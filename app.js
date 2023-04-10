aa7b933fd331aafbc8fe90418ac8a53d86e5c104
bilguun2411 <bilguundorj24@gmail.com>
2023-04-10 17:11:39 +0800

    Orlogo zarlaga delgetsen deer gargana

--- app.js
+++ app.js
@@ -16,15 +16,34 @@ var uiController = (function(){
             // addBtn: document.querySelector(DOMstrings.addBtn).value
             }
         },
+        
         domStrings: function(){
             return DOMstrings
+        },
+
+        addListItem: function(item,type){
+            //1. orlogog zarlagin elemnt aguulsan HTML -g beltgene
+            var html,list;
+            if (type==='inc') {
+                list = '.income__list'
+                html = '<div class="item clearfix" id="income-%id%"><div class="item__description">$$Description$$</div><div class="right clearfix"><div class="item__value">$$Value$$</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            } else {
+                list = '.expenses__list'
+                html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">$$Description$$</div><div class="right clearfix"><div class="item__value">$$Value$$</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            }
+            //2. ter HTML dotroo orlogo zarlgaiin utguudiig Replace ashiglan uurchlunu
+            html = html.replace('%id%', item.id);
+            html = html.replace('$$Description$$', item.description);
+            html = html.replace('$$Value$$', item.value);
+            //3. beltgesen HTML iin DOM ruu oruulj ugnu
+            document.querySelector(list).insertAdjacentHTML('beforeend',  html);
         }
     }
 })();
 
 //Sanhuutei ajillah controller
 var financeController = (function(){
-    var Income = function(id, description,value){
+    var Income  = function(id, description,value){
         this.id = id;
         this.description=description;
         this.value= value;
@@ -35,8 +54,8 @@ var financeController = (function(){
         this.value= value;
     };
 
-    i1 = new Income(1, "Salary",200)
-    i2 = new Expense(1,"cost",100)
+    // i1 = new Income(1, "Salary",200)
+    // i2 = new Expense(1,"cost",100)
 
     // var incomes = []
     // var expenses = []
@@ -55,23 +74,29 @@ var financeController = (function(){
         }
     }
     return {
-        addItem: function(type, desc,val){
-            
+        addItem: function(type, desc, val) {
+
             var item,id;
-            if( data.items[type].lenght === 0 ) {
-                id = 1; 
-            } else {
-                id = data.items[type][data.items[type].lenght-1] + 1 ;
-            };
+
+            if(data.items[type].length === 0) id = 1 ;
+            else {
+                id = data.items[type][data.items[type].length -1].id + 1;
+            }
+
+            
 
             if(type === "inc"){
-                item = new Income(id,desc,val);
+                item = new Income (id,desc,val);
             } else {
                 item = new Expense(id,desc,val); 
             };
 
             data.items[type].push(item);
+
+            return item;
+            
         },
+
         seeData: function(){
             return data;
         }
@@ -91,9 +116,10 @@ var appController = (function(uiController,financeController){
         var input = uiController.getInput();
         console.log(input)
         //2. olj avsan utgaa sanhuugiin controllert damjuulah
-        financeController.addItem(input.type,input.description,input.value);
+        var item = financeController.addItem(input.type,input.description,input.value);
         // console.log(financeController.data());
         //3. olj avsan utgaa web tohirgoo hesegt haragduulna
+        uiController.addListItem(item, input.type);
         //4. Tusviig tootsoolno
         //5. etssiin uldegdel tootsoolno
     };
